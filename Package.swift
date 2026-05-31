e7402b011f66052e1cc3586b9702088376c3fc19
Ritesh Pakala <rpakala@me.com>
2026-05-31 06:58:51 -0700

    update swift package deps, use frigate

--- Package.swift
+++ Package.swift
@@ -10,9 +10,7 @@ var packageDependencies: [Package.Dependency] = [
     .package(url: "https://github.com/grpc/grpc-swift-nio-transport.git", from: "1.0.0"),
     .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "1.0.0"),
     .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.28.0"),
-    .package(url: "https://github.com/riteshpakala/mlx-swift.git", branch: "gab/cuda1"),
-    .package(url: "https://github.com/riteshpakala/mlx.embeddings.git", branch: "main"),
-    .package(url: "https://github.com/riteshpakala/mlx-swift-lm", branch: "main"),
+    .package(url: "https://github.com/rao-studios/Frigate.git", branch: "main"),
 ]
 
 var targetDependencies: [Target.Dependency] = [
@@ -23,8 +21,9 @@ var targetDependencies: [Target.Dependency] = [
     .product(name: "GRPCNIOTransportHTTP2", package: "grpc-swift-nio-transport"),
     .product(name: "GRPCProtobuf", package: "grpc-swift-protobuf"),
     .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-    .product(name: "MLXLMCommon", package: "mlx-swift-lm"),
-    .product(name: "mlx_embeddings", package: "mlx.embeddings"),
+    .product(name: "MLX", package: "Frigate"),
+    .product(name: "MLXLMCommon", package: "Frigate"),
+    .product(name: "mlx_embeddings", package: "Frigate"),
 ]
 
 let supportedPlatforms: [SupportedPlatform] = [.macOS(.v15)]
